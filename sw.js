b37ea27f84002359111ae79122a02b4803f5cf47
frohty <frohty79@gmail.com>
2026-08-14 08:48:12 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+/* 오프라인에서도 열리도록 파일을 폰에 캐시해 둡니다 */
+const CACHE = 'maedeup-v13';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-maskable.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
